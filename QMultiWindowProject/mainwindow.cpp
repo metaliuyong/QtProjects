cb5dc22abb7b31f0a91cffea7ca788ffa93bbbfe
metaliuyong <metaliuyong@gmail.com>
2020-10-16 19:34:47 +0800

    add A qDebug

--- QMultiWindowProject/mainwindow.cpp
+++ QMultiWindowProject/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -7,6 +8,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     QWidget::setWindowFlag(Qt::FramelessWindowHint);
+    qDebug() << "Hello Git";
 }
 
 MainWindow::~MainWindow()
